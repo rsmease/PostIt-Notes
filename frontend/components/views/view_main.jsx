4182564181cfab8dd1d9fdc923930fe772e2af82
Ryan Mease <rsmease@gmail.com>
2018-02-04 11:44:58 -0800

    Add sass

--- frontend/components/views/view_main.jsx
+++ frontend/components/views/view_main.jsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+//components
+import HeaderAddNote from '../headers/header_add_note';
+
 export default () => (
-    <h1>Ryan's Note App</h1>
+    <HeaderAddNote />
 );
