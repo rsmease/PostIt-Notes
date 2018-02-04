53776a81e4e2b58394def47e04640a4f05c60f13
Ryan Mease <rsmease@gmail.com>
2018-02-04 10:56:50 -0800

    Remove testing from window in entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,16 +5,7 @@ import ReactDOM from 'react-dom';
 //component views
 import MainAppView from './components/views/main_app_view';
 
-//testing suite
-import * as RailsAPI from './rails_api/notes_api';
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    window.fetchNoteById = RailsAPI.fetchNoteById;
-    window.fetchNotes = RailsAPI.fetchNotes;
-    window.postNote = RailsAPI.postNote;
-    window.patchNote = RailsAPI.patchNote;
-    window.deleteNote = RailsAPI.deleteNote;
     const root = document.getElementById('root');
     ReactDOM.render(<MainAppView />, root);
 });
