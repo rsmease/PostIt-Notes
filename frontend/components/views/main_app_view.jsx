40230a40ace5232ddf705258c29deceede2e390b
Ryan Mease <rsmease@gmail.com>
2018-02-04 09:57:50 -0800

    Add main view sketch

---
+++ frontend/components/views/main_app_view.jsx
@@ -0,0 +1,7 @@
+//import
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+export default () => (
+    <h1>Ryan's Note App</h1>
+);
---
+++ frontend/entry.jsx
@@ -0,0 +1,12 @@
+//utils
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//component views
+import MainAppView from './components/views/main_app_view';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<MainAppView />, root);
+});
